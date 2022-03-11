5c4ad4db8ce23d4666fa9e67cad1c282a9c138f6
Michael Buffone <m_buffone@hotmail.com>
2022-03-10 21:08:53 -0500

    eslint

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -4,26 +4,26 @@ const { getUserByEmail } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail("user@example.com", testUsers)
+    const user = getUserByEmail("user@example.com", testUsers);
     const expectedUserID = "userRandomID";
     assert.equal(user.id, expectedUserID);
   });
   
   it('should return undefined when no user exists for a given email address', function() {
-    const user = getUserByEmail("m@m.com", testUsers)
+    const user = getUserByEmail("m@m.com", testUsers);
     const expectedUserID = undefined;
     assert.equal(user, expectedUserID);
   });
